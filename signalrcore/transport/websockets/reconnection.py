f18092c497480364204dad8d88e485000a2e5a04
Dominick Meglio <dmeglio@gmail.com>
2022-12-27 22:47:41 -0500

    Added exponential backoff support

--- signalrcore/transport/websockets/reconnection.py
+++ signalrcore/transport/websockets/reconnection.py
@@ -4,11 +4,7 @@ from enum import Enum
 
 
 class ConnectionStateChecker(object):
-    def __init__(
-            self,
-            ping_function,
-            keep_alive_interval,
-            sleep=1):
+    def __init__(self, ping_function, keep_alive_interval, sleep=1):
         self.sleep = sleep
         self.keep_alive_interval = keep_alive_interval
         self.last_message = time.time()
@@ -36,6 +32,7 @@ class ConnectionStateChecker(object):
 class ReconnectionType(Enum):
     raw = 0  # Reconnection with max reconnections and constant sleep time
     interval = 1  # variable sleep time
+    exponential = 2  # exponential backoff
 
 
 class ReconnectionHandler(object):
@@ -66,10 +63,37 @@ class RawReconnectionHandler(ReconnectionHandler):
                 return self.sleep_time
             else:
                 raise ValueError(
-                    "Max attemps reached {0}"
-                    .format(self.max_reconnection_attempts))
+                    "Max attempts reached {0}".format(self.max_reconnection_attempts)
+                )
         else:  # Infinite reconnect
             return self.sleep_time
+
+
+class ExponentialReconnectionHandler(ReconnectionHandler):
+    def __init__(self, sleep_time, max_interval, max_attempts):
+        super(ExponentialReconnectionHandler, self).__init__()
+        self.sleep_time = sleep_time
+        self.max_backoff = max_interval
+        self.max_reconnection_attempts = max_attempts
+
+    def next(self):
+        self.reconnecting = True
+        if (
+            self.max_reconnection_attempts is not None
+            and self.attempt_number >= self.max_reconnection_attempts
+        ):
+            raise ValueError(
+                "Max attempts reached {0}".format(self.max_reconnection_attempts)
+            )
+
+        proposed_sleep = self.sleep_time ** (self.attempt_number + 1)
+        self.attempt_number += 1
+        if self.max_backoff is not None and proposed_sleep > self.max_backoff:
+            proposed_sleep = self.max_backoff
+            # Prevent unnecessary crazy exponentiation that could be really slow
+            if self.max_reconnection_attempts is None:
+                self.attempt_number -= 1
+        return proposed_sleep
 
 
 class IntervalReconnectionHandler(ReconnectionHandler):
@@ -82,6 +106,5 @@ class IntervalReconnectionHandler(ReconnectionHandler):
         index = self.attempt_number
         self.attempt_number += 1
         if index >= len(self._intervals):
-            raise ValueError(
-                "Max intervals reached {0}".format(self._intervals))
+            raise ValueError("Max intervals reached {0}".format(self._intervals))
         return self._intervals[index]
