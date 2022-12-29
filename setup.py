38d206c05f3871c41aace4a32135bba168ab1845
Dominick Meglio <dmeglio@gmail.com>
2022-12-29 13:14:19 -0500

    Updated setup script

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="signalrcore",
-    version="0.9.6",
+    name="signalrcoreplus",
+    version="0.10.0",
     author="mandrewcito",
     author_email="anbaalo@gmail.com",
     description="A Python SignalR Core client(json and messagepack), with invocation auth and two way streaming. Compatible with azure / serverless functions. Also with automatic reconnect and manually reconnect.",
@@ -13,14 +13,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license_file="LICENSE",
-    url="https://github.com/mandrewcito/signalrcore",
+    url="https://github.com/dcmeglio/signalrcore",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3.6"
-    ],
-    install_requires=[
-        "requests>=2.22.0",
-        "websocket-client==1.0.0",
-        "msgpack==1.0.2"
-    ]
+    classifiers=["Programming Language :: Python :: 3.6"],
+    install_requires=["requests>=2.22.0", "websocket-client==1.4.2", "msgpack==1.0.2"],
 )
